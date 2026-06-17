eb29f7b6dd8083ac1f47a4e4ccd31c1418563a3b
[dyad] <pangesturayendra@gmail.com>
2026-06-17 10:04:53 +0700

    [dyad] wrote 1 file(s)

--- agent/collector/location_windows.go
+++ agent/collector/location_windows.go
@@ -31,10 +31,8 @@ type geoIPResponse struct {
 	Query       string  `json:"query"`
 }
 
-// GetLocationWindows attempts to get location via GeoIP on Windows.
-// Windows Geolocation API requires CGO + COM interop which adds complexity,
-// so we use ip-api.com as a reliable fallback that works on all platforms.
-func GetLocationWindows() (*LocationInfo, error) {
+// getPlatformLocation is called by location.go to get location on Windows.
+func getPlatformLocation() (*LocationInfo, error) {
 	client := &http.Client{Timeout: 5 * time.Second}
 
 	resp, err := client.Get("http://ip-api.com/json/?fields=status,country,countryCode,region,regionName,city,lat,lon,timezone,query")
@@ -55,7 +53,7 @@ func GetLocationWindows() (*LocationInfo, error) {
 	return &LocationInfo{
 		Latitude:       geo.Lat,
 		Longitude:      geo.Lon,
-		AccuracyMeters: 5000, // GeoIP accuracy is ~5km
+		AccuracyMeters: 5000,
 		Source:         "geoip",
 		City:           geo.City,
 		Country:        geo.Country,
